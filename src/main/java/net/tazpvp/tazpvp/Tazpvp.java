08d9b6bb4d806fd84f59696946aab9945593b7d1
professional-tdi <yogikngyt@gmail.com>
2022-03-01 17:31:57 -0500

    added basic stuff

--- src/main/java/net/tazpvp/tazpvp/Tazpvp.java
+++ src/main/java/net/tazpvp/tazpvp/Tazpvp.java
@@ -1,17 +1,38 @@
 package net.tazpvp.tazpvp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tazpvp extends JavaPlugin {
 
+    public static Tazpvp instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         // Plugin startup logic
+        Bukkit.getLogger().info(" Tazpvp has been enabled!");
+
+        registerCommands();
+        registerEvents();
+    }
+
+    public void registerCommands(){
+
+    }
+
+    public void registerEvents(){
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        Bukkit.getLogger().info(" Tazpvp has been disabled!");
+    }
+
+    public static Tazpvp getInstance(){
+        return instance;
     }
 }
