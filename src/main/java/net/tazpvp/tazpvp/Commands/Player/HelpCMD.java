4af3298951b687f1f4608856d50d51fa5366ba87
professional-tdi <yogikngyt@gmail.com>
2022-03-03 16:10:11 -0500

    moved over commands to logic instead of core

---
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/HelpCMD.java
@@ -0,0 +1,48 @@
+package net.tazpvp.tazpvp.Commands.Player;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelpCMD implements CommandExecutor {
+    String s1 = ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----" + ChatColor.RED + ChatColor.BOLD + "" + "TAZSPREE " + ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----\n" +
+            ChatColor.YELLOW + "Gain EXP from daily rewards, dealing damage,\n" +
+            ChatColor.YELLOW + "achievements, and killing mobs or players.\n" +
+            ChatColor.YELLOW + "\n" +
+            ChatColor.GOLD + "Gain money from killing, achievements, streaks,\n" +
+            ChatColor.GOLD + "mining, and daily rewards to use at the shop\n" +
+            ChatColor.YELLOW + "\n" +
+            ChatColor.RED + "Get up to five extra hearts from streaks,\n" +
+            ChatColor.RED + "rebirth, or getting the \"fat\" perk.\n" +
+            ChatColor.YELLOW + "\n" +
+            ChatColor.DARK_AQUA + "All inventory items are lost on death.\n" +
+            ChatColor.GREEN + "PAGE [1/2]" + ChatColor.GRAY + "/help2 for next page.";
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player p) {
+
+            if (strings.length >= 1) {
+                if (strings[0].equalsIgnoreCase("2")) {
+                    p.sendMessage(ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----" + ChatColor.RED + ChatColor.BOLD + "" + "TAZSPREE " + ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----\n" +
+                            ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.\n" +
+                            ChatColor.WHITE + "/apply " + ChatColor.GRAY + "Apply for a staff position.\n" +
+                            ChatColor.WHITE + "/spawn " + ChatColor.GRAY + "Teleport back to the spawn.\n" +
+                            ChatColor.WHITE + "/discord " + ChatColor.GRAY + "Join the community.\n" +
+                            ChatColor.WHITE + "/duel " + ChatColor.GRAY + "Battle versus another player.\n" +
+                            ChatColor.WHITE + "/ad " + ChatColor.GRAY + "Receive the server advertisement to copy.\n" +
+                            ChatColor.GRAY + "\n" +
+                            ChatColor.GREEN + "PAGE [2/2] " + ChatColor.GRAY + "/help to go back"
+                    );
+                } else {
+                    p.sendMessage(s1);
+                }
+            } else {
+                p.sendMessage(s1);
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/net/tazpvp/tazpvp/Tazpvp.java
+++ src/main/java/net/tazpvp/tazpvp/Tazpvp.java
@@ -1,5 +1,9 @@
 package net.tazpvp.tazpvp;
 
+import net.tazpvp.tazpvp.Commands.Player.HelpCMD;
+import net.tazpvp.tazpvp.Commands.Player.RulesCmd;
+import net.tazpvp.tazpvp.Events.DeathEvent;
+import net.tazpvp.tazpvp.Events.JoinEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,11 +29,13 @@ public final class Tazpvp extends JavaPlugin {
     }
 
     public void registerCommands(){
-
+        getCommand("help").setExecutor(new HelpCMD());
+        getCommand("rules").setExecutor(new RulesCmd());
     }
 
     public void registerEvents(){
-
+        getServer().getPluginManager().registerEvents(new DeathEvent(), this);
+        getServer().getPluginManager().registerEvents(new JoinEvent(), this);
     }
 
     public void initConfig(){
