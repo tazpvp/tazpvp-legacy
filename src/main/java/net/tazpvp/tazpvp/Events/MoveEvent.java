f352e2eeeb31fd48823a74b7776b0a4e13cfa7cb
rownox <rownox.ik@gmail.com>
2022-03-05 16:37:20 -0500

    huya

--- src/main/java/net/tazpvp/tazpvp/Events/MoveEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/MoveEvent.java
@@ -2,9 +2,11 @@ package net.tazpvp.tazpvp.Events;
 
 
 import net.tazpvp.tazpvp.Tazpvp;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -13,8 +15,9 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class MoveEvent implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
         Block b = new Location(e.getPlayer().getWorld(), e.getPlayer().getLocation().getX(), e.getPlayer().getLocation().getY() - 1, e.getPlayer().getLocation().getZ()).getBlock();
-        if (b.getType() == Material.WATER) {
+        if (b.getType() == Material.WATER && p.getGameMode() == GameMode.SURVIVAL) {
             for (int i = 0; i < e.getPlayer().getHealth(); i++) {
                 new BukkitRunnable() {
                     @Override
