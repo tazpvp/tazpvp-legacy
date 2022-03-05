8674c4f90ae86b7f73a84261d71c77e1729d4d2b
professional-tdi <yogikngyt@gmail.com>
2022-03-04 22:11:18 -0500

    r

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
@@ -13,7 +13,6 @@ public class ReportCMD {
     @CommandHook("report")
     public void report(Player p, Player target, String reason) {
         Bukkit.broadcast(ChatColor.DARK_GRAY +"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"+ChatColor.RED+""+ChatColor.BOLD +"REPORT " + ChatColor.WHITE + target.getName() + ChatColor.GRAY + " was reported for " + ChatColor.WHITE + reason + ChatColor.GRAY+" by "+ChatColor.WHITE + p.getName() + "\n" + ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "tazspree.staff.reports");
-
         p.sendMessage(ChatColor.DARK_AQUA + "Thank you for reporting " + ChatColor.WHITE + target.getName() + ChatColor.DARK_AQUA + " they will be reviewed shortly.");
     }
 }
