3e47fb4aac8f3836ffcd2adf0498bf17ef8ce8af
professional-tdi <yogikngyt@gmail.com>
2022-03-05 16:07:11 -0500

    new death way of dieing due to death of dying.

--- src/main/java/net/tazpvp/tazpvp/Events/MoveEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/MoveEvent.java
@@ -1,12 +1,14 @@
 package net.tazpvp.tazpvp.Events;
 
 
+import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class MoveEvent implements Listener {
     @EventHandler
@@ -14,8 +16,14 @@ public class MoveEvent implements Listener {
         Block b = new Location(e.getPlayer().getWorld(), e.getPlayer().getLocation().getX(), e.getPlayer().getLocation().getY() - 1, e.getPlayer().getLocation().getZ()).getBlock();
         if (b.getType() == Material.WATER) {
             for (int i = 0; i < e.getPlayer().getHealth(); i++) {
-                e.getPlayer().setHealth(e.getPlayer().getHealth() - 1);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        e.getPlayer().setHealth(e.getPlayer().getHealth() - 1);
+                    }
+                }.runTaskLater(Tazpvp.getInstance(), 1);
             }
+            e.getPlayer().setHealth(0);
         }
     }
 }
