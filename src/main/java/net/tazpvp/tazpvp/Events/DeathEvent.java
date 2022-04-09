213bd8ce7d9b41bfc6fa56b2e682e60b3d8a1d8f
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-08 23:32:23 -0400

    Updated the fitness gram pacer test

--- src/main/java/net/tazpvp/tazpvp/Events/DeathEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/DeathEvent.java
@@ -75,7 +75,7 @@ public class DeathEvent extends ekisBokisSerisEkis implements Listener {
             if (Tazpvp.bounty.containsKey(d.getUniqueId())) {
                 Tazpvp.statsManager.addMoney(p, Tazpvp.bounty.get(d.getUniqueId()));
                 Tazpvp.bounty.remove(d.getUniqueId());
-                p.sendMessage(ChatColor.GOLD + "You have received " + ChatColor.RED + Tazpvp.bounty.get(d.getUniqueId()) + ChatColor.GOLD + " for killing " + ChatColor.RED + d.getName());
+                p.sendMessage(ChatColor.GOLD + "You have received " + ChatColor.RED + Tazpvp.bounty.get(d.getUniqueId()) + ChatColor.GOLD + " for killing " + ChatColor.RED + d.getName()); /* The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start */ for (int i = 0; i < 100; i++) {p.getWorld().playEffect(p.getLocation().add(0, i, 0), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);p.getWorld().playEffect(p.getLocation().add(0, i, 0), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);p.getWorld().playEffect(p.getLocation().add(0, i, 0), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);p.getWorld().playEffect(p.getLocation().add(0, i, 0), Effect.STEP_SOUND, Material.REDSTONE_BLOCK);}
             }
 
             Tazpvp.statsManager.addKills(d, 1);
