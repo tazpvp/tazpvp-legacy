9f93648099503f78f350b3debe55e55e1e1528b0
rownox <90977516+rownox@users.noreply.github.com>
2022-04-15 08:44:13 -0400

    npc clibk

--- src/main/java/net/tazpvp/tazpvp/Events/NpcClickEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/NpcClickEvent.java
@@ -2,36 +2,12 @@ public class NPCClickEvent implements Listener {
 
     @EventHandler
     public void OnNPCLClick(NPCLeftClickEvent event) {
-        if(GUIManager.getGUI(event.getClicker()) != null) {
-            return;
-        }
-        if(event.getNPC().getId() == 7) { //SHOP
-            new GUIShop(event.getClicker());
-        } else if(event.getNPC().getId() == 15) { //ACHIEVEMENTS
-            new GUIAchievement(event.getClicker());
-        } else if(event.getNPC().getId() == 19) { //RANKS
-            new GUIMainRankScreen(event.getClicker());
-        } else if(event.getNPC().getId() == 32) { //UPGRADES
-            new GUIMainScreen(event.getClicker());
-        } else if (event.getNPC().getId() == 40) { //FISHERMAN
-            new Fisherman(event.getClicker());
-        }
+        return;
     }
     @EventHandler
     public void OnNPCRClick(NPCRightClickEvent event) {
-        if(GUIManager.getGUI(event.getClicker()) != null) {
-            return;
-        }
-        if(event.getNPC().getId() == 7) { //SHOP
-            new GUIShop(event.getClicker());
-        } else if(event.getNPC().getId() == 15) { //ACHIEVEMENTS
-            new GUIAchievement(event.getClicker());
-        } else if(event.getNPC().getId() == 19) { //RANKS
-            new GUIMainRankScreen(event.getClicker());
-        } else if(event.getNPC().getId() == 32) { //UPGRADES
-            new GUIMainScreen(event.getClicker());
-        } else if (event.getNPC().getId() == 40) { //FISHERMAN
-            new Fisherman(event.getClicker());
-        }
+        return;
     }
+
+
 }
