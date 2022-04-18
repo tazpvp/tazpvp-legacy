b26766cbc694cc7a37a06cf140107e72fe2abe33
rownox <90977516+rownox@users.noreply.github.com>
2022-04-18 12:16:55 -0400

    npcCmd

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/npcCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/npcCMD.java
@@ -1,1 +1,17 @@
-Villager v = (Villager) p.getWorld().spawnEntity(p.getLocation(), EntityType.VILLAGER);
+package net.tazpvp.tazpvp.Commands.Admin;
+
+import net.tazpvp.tazpvp.Utils.PlayerUtils;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import redempt.redlib.commandmanager.CommandHook;
+
+public class npcCMD {
+    public void npcCMD(CommandSender sender) {
+        if (sender instanceof Player p){
+            Villager v = (Villager) p.getWorld().spawnEntity(p.getLocation(), EntityType.VILLAGER);
+            v.setCustomName("YourCustomName");
+            p.sendMessage("Villager created!");
+            //TODO: change to redlib and register
+        }
+    }
+}
