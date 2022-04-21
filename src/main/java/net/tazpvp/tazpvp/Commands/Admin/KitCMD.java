5161ab7f46d4af7016987f1f58be5d6f9db471b9
rownox <rownox.ik@gmail.com>
2022-04-21 16:30:50 -0400

    dsfgbbrh

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/KitCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/KitCMD.java
@@ -1,7 +1,6 @@
 package net.tazpvp.tazpvp.Commands.Admin;
 
 import net.tazpvp.tazpvp.Utils.PlayerUtils;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import redempt.redlib.commandmanager.CommandHook;
 
--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/npcCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/npcCMD.java
@@ -1,10 +1,7 @@
 package net.tazpvp.tazpvp.Commands.Admin;
 
-import net.tazpvp.tazpvp.Utils.PlayerUtils;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import redempt.redlib.commandmanager.CommandHook;
@@ -14,6 +11,7 @@ public class npcCMD {
     public void npcCMD(Player p, String name) {
         Villager v = (Villager) p.getWorld().spawnEntity(p.getLocation(), EntityType.VILLAGER);
         v.setCustomName(ChatColor.translateAlternateColorCodes('&', name));
+        v.setAI(false);
         p.sendMessage("Villager created!");
     }
 }
