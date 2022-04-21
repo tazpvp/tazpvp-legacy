efd84bddff45dd77558a16edd1725e5f173ecfdc
rownox <rownox.ik@gmail.com>
2022-04-21 18:44:03 -0400

    wefoj

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/NpcCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/NpcCMD.java
@@ -8,10 +8,23 @@ import redempt.redlib.commandmanager.CommandHook;
 
 public class NpcCMD {
     @CommandHook("npc_create")
-    public void npcCMD(Player p, String name) {
+    public void npcCMD(Player p, String name, String profession, String type) {
         Villager v = (Villager) p.getWorld().spawnEntity(p.getLocation(), EntityType.VILLAGER);
         v.setCustomName(ChatColor.translateAlternateColorCodes('&', name));
+        v.setProfession(Villager.Profession.valueOf(profession));
+        v.setVillagerType(Villager.Type.valueOf(type));
         v.setAI(false);
         p.sendMessage("Villager created!");
+    }
+    @CommandHook("npc_remove")
+    public void npcRemoveCMD(Player p) {
+        p.getWorld().getEntities().stream().filter(e -> e instanceof Villager).forEach(e -> {
+            if (e.getCustomName() != null) {
+                if (e.getCustomName().equalsIgnoreCase(p.getName())) {
+                    e.remove();
+                }
+            }
+        });
+        p.sendMessage("Villager removed!");
     }
 }
