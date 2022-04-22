6bc5524b50a73bf4989ae5e7c325ff16d93c7696
rownox <rownox.ik@gmail.com>
2022-04-21 20:41:20 -0400

    test

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/AppealCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/AppealCMD.java
@@ -10,7 +10,7 @@ public class AppealCMD {
     @CommandHook("appeal")
     public void appeal(Player p){
         p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-        p.sendMessage(ChatColor.AQUA + "If you would like to be un-muted or un-banned, follow the steps below:");
+        p.sendMessage(ChatColor.AQUA + "If you would like to be un-muted or un-banned, please follow the steps below:");
         p.sendMessage(ChatColor.DARK_AQUA + "Contact support by going to the " + ChatColor.WHITE + "#support " + ChatColor.DARK_AQUA + "channel of our discord server.");
         p.sendMessage(ChatColor.DARK_AQUA + "False punishments will be quickly revoked; cheaters, however, will stay punished but can appeal 24 hours later.");
         p.spigot().sendMessage(DiscordUtils.discordInvite("Click Here to appeal."));
