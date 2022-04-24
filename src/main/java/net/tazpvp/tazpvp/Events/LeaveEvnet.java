68e3d639380aa7b8772d9dc669365c1830a29fb4
rownox <rownox.ik@gmail.com>
2022-04-24 14:24:31 -0400

    leave msg

--- src/main/java/net/tazpvp/tazpvp/Events/LeaveEvnet.java
+++ src/main/java/net/tazpvp/tazpvp/Events/LeaveEvnet.java
@@ -1,5 +1,7 @@
 package net.tazpvp.tazpvp.Events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -7,6 +9,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class LeaveEvnet implements Listener {
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
+        e.setQuitMessage(ChatColor.GRAY + "[" + ChatColor.RED + "-" + ChatColor.GRAY + "] " + e.getPlayer().getName());
 //        if (Tazpvp.bounty.containsKey(e.getPlayer().getUniqueId())) {
 //            Tazpvp.bounty.remove(e.getPlayer().getUniqueId());
 //        }
