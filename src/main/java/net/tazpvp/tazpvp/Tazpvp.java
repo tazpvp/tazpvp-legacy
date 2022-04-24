de0926783b2d6a3f9fe16f012fff31bf34c5be7f
rownox <rownox.ik@gmail.com>
2022-04-24 01:55:04 -0400

    tab

--- src/main/java/net/tazpvp/tazpvp/Tazpvp.java
+++ src/main/java/net/tazpvp/tazpvp/Tazpvp.java
@@ -257,7 +257,7 @@ public final class Tazpvp extends JavaPlugin {
     public static void sendBaseTablist(Player p) {
         p.setPlayerListHeaderFooter(
                 ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "TAZPVP\n",
-                ChatColor.GOLD + "\nIP: " + ChatColor.YELLOW + "tazpvp.net\n" + ChatColor.AQUA + Bukkit.getOnlinePlayers().size() + ChatColor.GRAY + "/" + ChatColor.DARK_AQUA + Bukkit.getMaxPlayers()
+                ChatColor.GOLD + "\nIP: " + ChatColor.YELLOW + "tazpvp.net\n" + "                                " +  ChatColor.AQUA + Bukkit.getOnlinePlayers().size() + ChatColor.GRAY + "/" + ChatColor.DARK_AQUA + Bukkit.getMaxPlayers()
         );
     }
 
