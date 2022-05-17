1fbda4c15b16fb1714d3aba95d3c6f0c7cf8def1
rownox <rownox.ik@gmail.com>
2022-05-16 21:11:30 -0400

    join smsg

--- src/main/java/net/tazpvp/tazpvp/Events/JoinEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/JoinEvent.java
@@ -18,14 +18,20 @@ public class JoinEvent implements Listener {
         Player p = e.getPlayer();
         Location spawn = configUtils.spawn;
 
+        p.sendMessage("▍  " + ChatColor.RED + "TAZPVP");
+        p.sendMessage("▍  " + ChatColor.GRAY + "Type /discord");
+        p.sendMessage("▍  " + ChatColor.GRAY + "IP: tazpvp.net");
+        p.sendMessage("");
+
         if (p.hasPlayedBefore()) {
             e.setJoinMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "+" + ChatColor.GRAY + "] " + p.getName());
         } else {
             Tazpvp.statsManager.initPlayer(p);
             e.setJoinMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "+" + ChatColor.GRAY + "] " + p.getName());
-            p.sendTitle(ChatColor.GOLD + "Welcome to " + ChatColor.YELLOW + "TazSpree", ChatColor.GOLD + "Type /help to get started", 10, 100, 10);
+            p.sendTitle(ChatColor.GOLD + "Welcome to " + ChatColor.YELLOW + "TazPvP", ChatColor.GOLD + "Type /help to get started", 10, 100, 10);
             PlayerUtils.kitPlayer(p);
         }
+
         p.teleport(spawn);
 
         for(Scoreboard sb : Tazpvp.statsManager.scoreboards.values()) {
