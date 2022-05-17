158111f5c4b83e6146362985b14e0327aaf76954
rownox <rownox.ik@gmail.com>
2022-05-16 20:24:08 -0400

    ficksed man command

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/GuiCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/GuiCMD.java
@@ -1,5 +1,6 @@
 package net.tazpvp.tazpvp.Commands.Admin;
 
+import net.tazpvp.tazpvp.GUI.MainMenu.MainGUI;
 import net.tazpvp.tazpvp.unused.FlameVilGUI;
 import net.tazpvp.tazpvp.GUI.ShopGUI;
 import net.tazpvp.tazpvp.GUI.TestGUI;
@@ -13,8 +14,8 @@ public class GuiCMD {
             new TestGUI(p);
         } else if (gui.equalsIgnoreCase("shop")) {
             new ShopGUI(p);
-        } else if (gui.equalsIgnoreCase("upgrade")) {
-            new MainMenuGUI(p);
+        } else if (gui.equalsIgnoreCase("main")) {
+            new MainGUI(p);
         } else if (gui.equalsIgnoreCase("flame")) {
             new FlameVilGUI(p);
         }
