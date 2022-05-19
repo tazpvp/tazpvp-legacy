bf5d517d51a166cf511a7ce46a3bd1345bbd75fb
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-05-19 19:24:19 -0400

    yes

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/PWCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/PWCMD.java
@@ -1,14 +1,17 @@
 package net.tazpvp.tazpvp.Commands.Admin;
 
 import net.tazpvp.tazpvp.Managers.PlayerWrapper;
+import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.entity.Player;
 import redempt.redlib.commandmanager.CommandHook;
 
 public class PWCMD {
     @CommandHook("pw_test")
     public void pwTest(Player p) {
-        PlayerWrapper pw = new PlayerWrapper(p);
+        PlayerWrapper pw = Tazpvp.playerWrapperStatsManager.getPlayerWrapper(p);
 
         pw.sendMother();
+
+        Tazpvp.playerWrapperStatsManager.setPlayerWrapper(p, pw);
     }
 }
