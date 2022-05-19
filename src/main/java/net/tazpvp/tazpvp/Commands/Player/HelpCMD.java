ab4502620da31353a87063d8fed69e3044fe3767
rownox <rownox.ik@gmail.com>
2022-05-19 16:53:54 -0400

    help

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/HelpCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/HelpCMD.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import redempt.redlib.commandmanager.CommandHook;
 
 public class HelpCMD {
-    String s1 = ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----" + ChatColor.RED + ChatColor.BOLD + "" + "TAZPVP " + ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----\n" +
+    String s1 = "" +
             ChatColor.YELLOW + "Gain EXP from daily rewards, dealing damage,\n" +
             ChatColor.YELLOW + "achievements, and killing mobs or players.\n" +
             ChatColor.YELLOW + "\n" +
@@ -16,14 +16,15 @@ public class HelpCMD {
             ChatColor.RED + "rebirth, or getting the \"fat\" perk.\n" +
             ChatColor.YELLOW + "\n" +
             ChatColor.DARK_AQUA + "All inventory items are lost on death.\n" +
-            ChatColor.GREEN + "PAGE [1/2]" + ChatColor.GRAY + "/help2 for next page.";
+            "" +
+            ChatColor.GREEN + "PAGE [1/2]" + ChatColor.GRAY + " type '/help 2' for next page.";
 
     @CommandHook("help")
     public void help(Player p, int page) {
         if (page == 1) {
             p.sendMessage(s1);
         } else if (page == 2) {
-            p.sendMessage(ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----" + ChatColor.RED + ChatColor.BOLD + "" + "TAZPVP " + ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----\n" +
+            p.sendMessage("" +
                     ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.\n" +
                     ChatColor.WHITE + "/apply " + ChatColor.GRAY + "Apply for a staff position.\n" +
                     ChatColor.WHITE + "/spawn " + ChatColor.GRAY + "Teleport back to the spawn.\n" +
@@ -31,7 +32,7 @@ public class HelpCMD {
                     ChatColor.WHITE + "/duel " + ChatColor.GRAY + "Battle versus another player.\n" +
                     ChatColor.WHITE + "/ad " + ChatColor.GRAY + "Receive the server advertisement to copy.\n" +
                     ChatColor.GRAY + "\n" +
-                    ChatColor.GREEN + "PAGE [2/2] " + ChatColor.GRAY + "/help to go back"
+                    ChatColor.GREEN + "PAGE [2/2] " + ChatColor.GRAY + "type '/help' to go back"
             );
         } else {
             p.sendMessage(ChatColor.RED + "Invalid page.");
