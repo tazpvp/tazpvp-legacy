fb50830ef3bf10a1e8bd5a056e0871eab311a74d
rownox <rownox.ik@gmail.com>
2022-05-21 16:22:42 -0400

    fixed 6

--- src/main/java/net/tazpvp/tazpvp/Custom/buyRank.java
+++ src/main/java/net/tazpvp/tazpvp/Custom/buyRank.java
@@ -45,7 +45,7 @@ public class buyRank implements Listener {
 
     public static void RankBuying(Player p) {
         p.sendMessage("Gift or buy?");
-        Bukkit.broadcastMessage("g");
+        Bukkit.broadcastMessage(p.getName());
         Tazpvp.Buying.add(p.getName());
         Bukkit.broadcastMessage(Tazpvp.Buying.get(1));
         new BukkitRunnable() {
