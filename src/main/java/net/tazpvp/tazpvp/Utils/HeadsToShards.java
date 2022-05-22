97874d6fe27c4d00b7a8cb2f43806d8fc7f059a0
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-05-22 18:15:15 -0400

    head

--- src/main/java/net/tazpvp/tazpvp/Utils/HeadsToShards.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/HeadsToShards.java
@@ -6,20 +6,29 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import redempt.redlib.itemutils.ItemBuilder;
 
 public class HeadsToShards {
     String prefix = ChatColor.YELLOW + "[NPC] Bub: " + ChatColor.WHITE;
     public void convertHeadsToShards(Player p) {
         if (p.getInventory().getItemInMainHand().getType().equals(Material.PLAYER_HEAD)) {
             ItemStack item = p.getInventory().getItemInMainHand();
-            Tazpvp.statsManager.addShards(p, item.getAmount());
-            p.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+            ItemStack ie = new ItemBuilder(Material.PRISMARINE_SHARD).setName(ChatColor.AQUA + "Shard").setCount(item.getAmount());
+            p.getInventory().setItemInMainHand(ie);
             for (int i = 0; i < item.getAmount(); i++) {
                 p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
             }
             p.sendMessage(prefix + "Traded in " + item.getAmount() + " heads for " + item.getAmount() + " shards.");
+        } else if (p.getInventory().getItemInMainHand().getType().equals(Material.PRISMARINE_SHARD)) {
+            ItemStack item = p.getInventory().getItemInMainHand();
+            p.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+            Tazpvp.statsManager.addShards(p, item.getAmount());
+            for (int i = 0; i < item.getAmount(); i++) {
+                p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+            }
+            p.sendMessage(prefix + "Traded in " + item.getAmount() + " shards for " + item.getAmount() + " digital shards.");
         } else {
-            p.sendMessage(prefix + "I trade head for shards, hold them in your hand next time bud.");
+            p.sendMessage(prefix + "I trade head for shards and shards for digital shards, hold them in your hand next time bud.");
             p.playSound(p.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
         }
     }
