819742c8e88b25fe59e0639774b4cb0e57ad4af1
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-05-21 21:20:57 -0400

    rownox forgot to handle many things

--- src/main/java/net/tazpvp/tazpvp/Events/ClickBlockEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/ClickBlockEvent.java
@@ -4,17 +4,19 @@ import net.tazpvp.tazpvp.GUI.UnlockSwordGUI;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ClickBlockEvent implements Listener {
+    @EventHandler
     public void blockClick(PlayerInteractEvent e){
         Player p = e.getPlayer();
-        Block b = e.getClickedBlock();
-        Material block = b.getType();
-        if (block == Material.RESPAWN_ANCHOR){
-            if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block b = e.getClickedBlock();
+            assert b != null;
+            if (b.getType() == Material.RESPAWN_ANCHOR) {
                 new UnlockSwordGUI(p);
             }
         }
