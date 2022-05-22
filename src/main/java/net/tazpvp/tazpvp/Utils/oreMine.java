1a7a5bf30163b297ceb546175dc81d59d4a8232d
rownox <rownox.ik@gmail.com>
2022-05-22 10:52:05 -0400

    izshjgsziuo

--- src/main/java/net/tazpvp/tazpvp/Utils/oreMine.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/oreMine.java
@@ -17,37 +17,26 @@ public class oreMine implements Listener {
     public void onBlockBreak(org.bukkit.event.block.BlockBreakEvent e) {
         Player p = e.getPlayer();
         if (p.getGameMode() == GameMode.SURVIVAL) {
+            e.setCancelled(true);
             Block b = e.getBlock();
             Material block = b.getType();
-            Bukkit.broadcastMessage("1");
             if (Tazpvp.blocks.contains(block)) {
-                Bukkit.broadcastMessage("2");
-                if (block == Material.DEEPSLATE_GOLD_ORE) {
-                    Bukkit.broadcastMessage("3");
-                    oreRespawn(p, block, Material.RAW_GOLD, 350L, b);
-                } else if (block == Material.DEEPSLATE_REDSTONE_ORE) {
-                    oreRespawn(p, block, Material.REDSTONE, 350L, b);
-                } else if (block == Material.DEEPSLATE_IRON_ORE) {
-                    oreRespawn(p, block, Material.RAW_IRON, 350L, b);
-                } else if (block == Material.DEEPSLATE_LAPIS_ORE) {
-                    oreRespawn(p, block, Material.LAPIS_LAZULI, 350L, b);
-                } else if (block == Material.DEEPSLATE_EMERALD_ORE) {
-                    oreRespawn(p, block, Material.EMERALD, 350L, b);
+                if (block == Material.DEEPSLATE_GOLD_ORE) { oreRespawn(p, block, Material.RAW_GOLD, 350L, b);
+                } else if (block == Material.DEEPSLATE_REDSTONE_ORE) { oreRespawn(p, block, Material.REDSTONE, 350L, b);
+                } else if (block == Material.DEEPSLATE_IRON_ORE) { oreRespawn(p, block, Material.RAW_IRON, 350L, b);
+                } else if (block == Material.DEEPSLATE_LAPIS_ORE) { oreRespawn(p, block, Material.LAPIS_LAZULI, 350L, b);
+                } else if (block == Material.DEEPSLATE_EMERALD_ORE) { oreRespawn(p, block, Material.EMERALD, 350L, b);
                 }
             }
-            e.setCancelled(true);
         }
     }
-
     public void oreRespawn(Player p, Material block, Material block2, Long dur, Block b){
         p.getInventory().addItem(new ItemStack(block));
-        Bukkit.broadcastMessage("4");
         b.setType(Material.BEDROCK);
         new BukkitRunnable() {
             @Override
             public void run() {
                 b.setType(block);
-                Bukkit.broadcastMessage("4");
             }
         }.runTaskLater(Tazpvp.getInstance(), dur);
     }
