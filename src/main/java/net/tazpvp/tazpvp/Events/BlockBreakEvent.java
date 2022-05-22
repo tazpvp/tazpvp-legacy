bfc75348a0f579358001192a3ad8cd7e7cbbbca6
rownox <rownox.ik@gmail.com>
2022-05-22 11:08:40 -0400

    fix

--- src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
@@ -1,5 +1,7 @@
 package net.tazpvp.tazpvp.Events;
 
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +15,12 @@ public class BlockBreakEvent implements Listener {
     @EventHandler
     public void onBlockBreak(org.bukkit.event.block.BlockBreakEvent e) {
         Player p = e.getPlayer();
+        if(p.getGameMode() == GameMode.SURVIVAL) {
+            Block b = e.getBlock();
+            if (!isPlayerPlaced(b)) {
+                e.setCancelled(true);
+            }
+        }
     }
 
     public boolean isPlayerPlaced(Block b){
