416eddbab99d0685b18ec5990e0b9cd41b2aa0ac
rownox <rownox.ik@gmail.com>
2022-05-22 11:22:10 -0400

    oremine cleanup

--- src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
@@ -1,5 +1,6 @@
 package net.tazpvp.tazpvp.Events;
 
+import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -10,6 +11,8 @@ import org.bukkit.metadata.MetadataValue;
 
 import java.util.List;
 
+import static net.tazpvp.tazpvp.Utils.oreMine.oreRespawn;
+
 public class BlockBreakEvent implements Listener {
 
     @EventHandler
@@ -17,8 +20,17 @@ public class BlockBreakEvent implements Listener {
         Player p = e.getPlayer();
         if(p.getGameMode() == GameMode.SURVIVAL) {
             Block b = e.getBlock();
-            if (!isPlayerPlaced(b)) {
+            Material block = b.getType();
+            if (isPlayerPlaced(b)) { e.setCancelled(false);
+            } else { e.setCancelled(true);}
+            if (Tazpvp.blocks.contains(block)) {
                 e.setCancelled(true);
+                if (block == Material.DEEPSLATE_GOLD_ORE) { oreRespawn(p, block, Material.RAW_GOLD, 350L, b);
+                } else if (block == Material.DEEPSLATE_REDSTONE_ORE) { oreRespawn(p, block, Material.REDSTONE, 350L, b);
+                } else if (block == Material.DEEPSLATE_IRON_ORE) { oreRespawn(p, block, Material.RAW_IRON, 350L, b);
+                } else if (block == Material.DEEPSLATE_LAPIS_ORE) { oreRespawn(p, block, Material.LAPIS_LAZULI, 350L, b);
+                } else if (block == Material.DEEPSLATE_EMERALD_ORE) { oreRespawn(p, block, Material.EMERALD, 350L, b);
+                }
             }
         }
     }
--- src/main/java/net/tazpvp/tazpvp/Utils/oreMine.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/oreMine.java
@@ -12,25 +12,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class oreMine implements Listener {
-
-    @EventHandler
-    public void onBlockBreak(org.bukkit.event.block.BlockBreakEvent e) {
-        Player p = e.getPlayer();
-        if (p.getGameMode() == GameMode.SURVIVAL) {
-            Block b = e.getBlock();
-            Material block = b.getType();
-            if (Tazpvp.blocks.contains(block)) {
-                e.setCancelled(true);
-                if (block == Material.DEEPSLATE_GOLD_ORE) { oreRespawn(p, block, Material.RAW_GOLD, 350L, b);
-                } else if (block == Material.DEEPSLATE_REDSTONE_ORE) { oreRespawn(p, block, Material.REDSTONE, 350L, b);
-                } else if (block == Material.DEEPSLATE_IRON_ORE) { oreRespawn(p, block, Material.RAW_IRON, 350L, b);
-                } else if (block == Material.DEEPSLATE_LAPIS_ORE) { oreRespawn(p, block, Material.LAPIS_LAZULI, 350L, b);
-                } else if (block == Material.DEEPSLATE_EMERALD_ORE) { oreRespawn(p, block, Material.EMERALD, 350L, b);
-                }
-            }
-        }
-    }
-    public void oreRespawn(Player p, Material block, Material block2, Long dur, Block b){
+    public static void oreRespawn(Player p, Material block, Material block2, Long dur, Block b){
         p.getInventory().addItem(new ItemStack(block));
         b.setType(Material.BEDROCK);
         new BukkitRunnable() {
