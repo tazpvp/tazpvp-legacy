57815c9e0ed415b8b64b59d4fe5b78ad94d2aaff
rownox <rownox.ik@gmail.com>
2022-05-22 00:23:49 -0400

    bh

--- src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/BlockBreakEvent.java
@@ -7,8 +7,13 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
 
 public class BlockBreakEvent implements Listener {
 
@@ -48,5 +53,22 @@ public class BlockBreakEvent implements Listener {
             }
         }.runTaskLater(Tazpvp.getInstance(), dur);
     }
+
+//    @EventHandler
+//    public void placeBlock(BlockPlaceEvent e){
+//        if (e.getPlayer().getGameMode() == GameMode.SURVIVAL && e.getPlayer().getWorld().getName().equalsIgnoreCase("arena")){
+//            Player p = e.getPlayer();
+//            Block b = e.getBlock();
+//            b.setMetadata("PlayerPlaced", new FixedMetadataValue(TazPvP.getInstance(), true));
+//        }
+//    }
+//
+//    public boolean isPlayerPlaced(Block b){
+//        List<MetadataValue> metaDataValues = b.getMetadata("PlayerPlaced");
+//        for (MetadataValue metaDataValue : metaDataValues) {
+//            return metaDataValue.asBoolean();
+//        }
+//        return false;
+//    }
 
 }
