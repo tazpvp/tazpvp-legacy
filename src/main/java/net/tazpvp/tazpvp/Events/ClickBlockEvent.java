0fbf0386a61da0be4ed79db56cdd4486f72c0e55
rownox <rownox.ik@gmail.com>
2022-05-21 20:51:52 -0400

    committed sui- i mean update

---
+++ src/main/java/net/tazpvp/tazpvp/Events/ClickBlockEvent.java
@@ -0,0 +1,21 @@
+package net.tazpvp.tazpvp.Events;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ClickBlockEvent implements Listener {
+    public void blockClick(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        Block b = e.getClickedBlock();
+        Material block = b.getType();
+        if (block == Material.RESPAWN_ANCHOR){
+            if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+//                new UnlockSwordGUI(p);
+            }
+        }
+    }
+}
