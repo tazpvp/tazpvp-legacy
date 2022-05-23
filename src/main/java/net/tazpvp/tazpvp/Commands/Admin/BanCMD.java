cadf5e23b2e8691ebd5e828a5fe2eef66fd33f7e
rownox <90977516+rownox@users.noreply.github.com>
2022-05-23 07:28:04 -0400

    start of ban cmd

---
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/BanCMD.java
@@ -0,0 +1,31 @@
+package net.tazpvp.tazpvp.Commands.Admin;
+
+import net.tazpvp.tazpvp.Tazpvp;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import redempt.redlib.commandmanager.CommandHook;
+
+public class MuteCMD {
+    @CommandHook("ban")
+    public void mute(CommandSender sender, Player target) {
+        if (!Tazpvp.punishmentManager.isMuted(target)) {
+            Tazpvp.punishmentManager.initMute(target, true);
+            sender.sendMessage(ChatColor.GOLD + "You have muted " + ChatColor.RED + target.getName());
+            target.sendMessage(ChatColor.GOLD + "You have been muted");
+        } else {
+            sender.sendMessage(ChatColor.RED + target.getName() + ChatColor.GOLD + " is already muted");
+        }
+    }
+
+    @CommandHook("unban")
+    public void unmute(CommandSender sender, Player target) {
+        if (Tazpvp.punishmentManager.isMuted(target)) {
+            Tazpvp.punishmentManager.removeMute(target);
+            sender.sendMessage(ChatColor.GOLD + "You have unmuted " + ChatColor.RED + target.getName());
+            target.sendMessage(ChatColor.GOLD + "You have been unmuted");
+        } else {
+            sender.sendMessage(ChatColor.RED + target.getName() + ChatColor.GOLD + " is not muted");
+        }
+    }
+}
