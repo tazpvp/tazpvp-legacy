6e8da12aaf2331344b9f23caa4483466dd58389a
rownox <rownox.ik@gmail.com>
2022-05-24 19:31:48 -0400

    wsejug

--- src/main/java/net/tazpvp/tazpvp/GUI/MainMenu/MainGUI.java
+++ src/main/java/net/tazpvp/tazpvp/GUI/MainMenu/MainGUI.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import redempt.redlib.inventorygui.InventoryGUI;
 import redempt.redlib.inventorygui.ItemButton;
+import redempt.redlib.itemutils.ItemBuilder;
 
 public class MainGUI {
     private final InventoryGUI gui;
@@ -20,19 +21,19 @@ public class MainGUI {
 
     public void setitems(){
         gui.fill(0, 27, new ItemStack(Material.BLACK_STAINED_GLASS_PANE));
-        ItemButton achievements = ItemButton.create(new ItemStack(Material.WRITABLE_BOOK), e -> {
+        ItemButton achievements = ItemButton.create(new ItemBuilder(Material.WRITABLE_BOOK).setName("Achievements"), e -> {
             Player p = (Player) e.getWhoClicked();
             p.closeInventory();
             new AchievementsGUI(p);
         });
         gui.addButton(11, achievements);
-        ItemButton collection = ItemButton.create(new ItemStack(Material.DIAMOND_SWORD), e -> {
+        ItemButton collection = ItemButton.create(new ItemBuilder(Material.DIAMOND_SWORD).setName("Collection"), e -> {
             Player p = (Player) e.getWhoClicked();
             p.closeInventory();
             new SwordCollection(p);
         });
         gui.addButton(13, collection);
-        ItemButton store = ItemButton.create(new ItemStack(Material.TNT_MINECART), e -> {
+        ItemButton store = ItemButton.create(new ItemBuilder(Material.TNT_MINECART).setName("Store").setLore("Ranks and Cosmetics"), e -> {
             Player p = (Player) e.getWhoClicked();
             p.closeInventory();
 
