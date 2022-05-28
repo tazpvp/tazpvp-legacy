268c3e537a14e8fcf21192e814e9d64b0747836c
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-05-28 18:27:30 -0400

    forgot to add lol z

--- src/main/java/net/tazpvp/tazpvp/DiscordBot/StartBotThread.java
+++ src/main/java/net/tazpvp/tazpvp/DiscordBot/StartBotThread.java
@@ -1,7 +1,13 @@
 package net.tazpvp.tazpvp.DiscordBot;
 
+import javax.security.auth.login.LoginException;
+
 public class StartBotThread extends Thread{
     public void run() {
-
+        try {
+            TazBot.main(null);
+        } catch (LoginException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
