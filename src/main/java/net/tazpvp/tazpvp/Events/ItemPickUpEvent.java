0c5876caaf671d25ce114567550c36d81e0ad293
rownox <rownox.ik@gmail.com>
2022-05-30 16:29:11 -0400

    fixed ntdi's code

--- src/main/java/net/tazpvp/tazpvp/Events/ItemPickUpEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/ItemPickUpEvent.java
@@ -1,20 +1,30 @@
 package net.tazpvp.tazpvp.Events;
 
 import net.tazpvp.tazpvp.Tazpvp;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ItemPickUpEvent implements Listener {
     @EventHandler
     public void onItemPickUp(EntityPickupItemEvent e) {
         if (e.getEntity() instanceof Player p) {
-            if (e.getItem().getItemStack().getType() == Material.PRISMARINE_SHARD) {
-                p.getInventory().remove(Material.PRISMARINE_SHARD);
-                Tazpvp.statsManager.addShards(p, 1);
+            if (e.getItem().getItemStack().getType() == Material.AMETHYST_SHARD) {
+                new BukkitRunnable(){
+                    public void run(){
+                        p.getInventory().remove(Material.AMETHYST_SHARD);
+                        p.sendMessage(ChatColor.DARK_AQUA + "You picked up a shard. " + ChatColor.AQUA + "+1 Shard");
+                        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1, 1);
+                        Tazpvp.statsManager.addShards(p, 1);
+
+                    }
+                }.runTaskLater(Tazpvp.getInstance(), 5);
             }
         }
     }
