c4d94a653469c63d89de131fbdf612b3e60dcdb5
rownox <rownox.ik@gmail.com>
2022-05-30 02:48:24 -0400

    scuffed but works

--- src/main/java/net/tazpvp/tazpvp/GUI/MainMenu/SubMenu/ServerStore.java
+++ src/main/java/net/tazpvp/tazpvp/GUI/MainMenu/SubMenu/ServerStore.java
@@ -22,11 +22,12 @@ public class ServerStore {
         gui.open(p);
     }
 
-    public void createShopButton(ItemStack item, int slot, int price, String name, String description){
-        ItemBuilder b = new ItemBuilder(item).setName(name).setLore(description, ChatColor.GOLD + "Cost: " + ChatColor.GRAY + "$" + price);
+    public void createShopButton(ItemBuilder b, int slot, int price){
         ItemButton button = ItemButton.create(b, e -> {
             Player p = (Player) e.getWhoClicked();
+            p.closeInventory();
             RankBuying(p, price);
+            buyRank.rank = "vip";
         });
         gui.addButton(slot, button);
     }
@@ -34,28 +35,54 @@ public class ServerStore {
     public void setitems(){
         gui.fill(0, 9*5, new ItemStack(Material.BLACK_STAINED_GLASS_PANE));
 
-        ItemBuilder VIP
-        ItemButton VIP = ItemButton.create(new ItemBuilder(Material.MUSIC_DISC_CHIRP).setName(ChatColor.RED + "" + ChatColor.BOLD + "VIP " + ChatColor.GRAY + "250 Credits").setLore(vip), e -> {
-            Player p = (Player) e.getWhoClicked();
-            p.closeInventory();
-            RankBuying(p);
-            buyRank.rank = "vip";
-        });
-        gui.addButton(11, VIP);
-        ItemButton MVP = ItemButton.create(new ItemBuilder(Material.MUSIC_DISC_WAIT).setName(ChatColor.RED + "" + ChatColor.BOLD + "MVP " + ChatColor.GRAY + "250 Credits"), e -> {
-            Player p = (Player) e.getWhoClicked();
-            p.closeInventory();
-            RankBuying(p);
-            buyRank.rank = "mvp";
-        });
-        gui.addButton(12, MVP);
-        ItemButton MVP2 = ItemButton.create(new ItemBuilder(Material.MUSIC_DISC_PIGSTEP).setName(ChatColor.RED + "" + ChatColor.BOLD + "MVP+ " + ChatColor.GRAY + "250 Credits"), e -> {
-            Player p = (Player) e.getWhoClicked();
-            p.closeInventory();
-            RankBuying(p);
-            buyRank.rank = "mvp+";
-        });
-        gui.addButton(13, MVP2);
+        ItemBuilder vip = new ItemBuilder(Material.MUSIC_DISC_CHIRP).setName(ChatColor.RED+""+ChatColor.BOLD+"VIP " + ChatColor.GRAY + 250 + " Credits")
+                .setLore(ChatColor.RED+
+                        "〡Enderchest access",
+                        "〡Hat command",
+                        "〡Nickname command",
+                        "〡Votekick command",
+                        "〡Invsee command",
+                        "〡Vanish command",
+                        "〡RGB Armor Colors",
+                        "〡Durable Blocks",
+                        "〡Anti-spam Bypass",
+                        "〡No Lag-back",
+                        "〡RGB Blocks",
+                        "〡VIP++ discord rank"
+                );
+        createShopButton(vip, 11, 250);
+        ItemBuilder mvp = new ItemBuilder(Material.MUSIC_DISC_WAIT).setName(ChatColor.GREEN+""+ChatColor.BOLD+"MVP " + ChatColor.GRAY + 450 + " Credits")
+                .setLore(ChatColor.GREEN+
+                        "〡Enderchest access",
+                        "〡Hat command",
+                        "〡Nickname command",
+                        "〡Votekick command",
+                        "〡Invsee command",
+                        "〡Vanish command",
+                        "〡RGB Armor Colors",
+                        "〡Durable Blocks",
+                        "〡Anti-spam Bypass",
+                        "〡No Lag-back",
+                        "〡RGB Blocks",
+                        "〡VIP++ discord rank"
+                );
+        createShopButton(mvp, 12, 250);
+        ItemBuilder mvp2 = new ItemBuilder(Material.MUSIC_DISC_PIGSTEP).setName(ChatColor.GOLD+""+ChatColor.BOLD+"MVP+ " + ChatColor.GRAY + 950 + " Credits")
+                .setLore(ChatColor.GOLD+
+                        "〡Enderchest access",
+                        "〡Hat command",
+                        "〡Nickname command",
+                        "〡Votekick command",
+                        "〡Invsee command",
+                        "〡Vanish command",
+                        "〡RGB Armor Colors",
+                        "〡Durable Blocks",
+                        "〡Anti-spam Bypass",
+                        "〡No Lag-back",
+                        "〡RGB Blocks",
+                        "〡VIP++ discord rank"
+                );
+        createShopButton(mvp2, 13, 250);
         ItemButton UNBAN = ItemButton.create(new ItemBuilder(Material.TNT_MINECART).setName(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN").setLore(ChatColor.GRAY + "Purchase an unban.\n" + ChatColor.DARK_AQUA + "100 Credits"), e -> {
             Player p = (Player) e.getWhoClicked();
             p.closeInventory();
