a8ebb7d7dc812d75e39493c9dbe9cf902b45cf96
Ntdi <yogikngyt@gmail.com>
2022-06-02 14:30:46 -0400

    guild start

---
+++ src/main/java/net/tazpvp/tazpvp/Managers/GuildWrapperManager/GuildWrapper.java
@@ -0,0 +1,10 @@
+package net.tazpvp.tazpvp.Managers.GuildWrapperManager;
+
+import java.util.UUID;
+
+public class GuildWrapper {
+    private final String name;
+    private String name;
+    private String tag;
+    private UUID owner;
+}
