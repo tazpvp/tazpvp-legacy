a45fb7f86a7b285a9f4c3135484fe72b8b1dc81b
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-02 17:44:49 -0400

    temp

--- src/main/java/net/tazpvp/tazpvp/Managers/GuildWrapperManager/GuildWrapper.java
+++ src/main/java/net/tazpvp/tazpvp/Managers/GuildWrapperManager/GuildWrapper.java
@@ -3,8 +3,8 @@ package net.tazpvp.tazpvp.Managers.GuildWrapperManager;
 import java.util.UUID;
 
 public class GuildWrapper {
-    private final String name;
-    private String name;
-    private String tag;
-    private UUID owner;
+//    private final String name;
+//    private String name;
+//    private String tag;
+//    private UUID owner;
 }
