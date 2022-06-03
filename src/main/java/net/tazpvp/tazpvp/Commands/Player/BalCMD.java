375b0be9094daf68069a5b0653ccf9756895c220
Ntdi <yogikngyt@gmail.com>
2022-06-03 09:53:17 -0400

    pay command

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/BalCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/BalCMD.java
@@ -13,4 +13,16 @@ public class BalCMD implements CommandListener {
         int bal = Tazpvp.statsManager.getMoney(target);
         sender.sendMessage(ChatColor.YELLOW + target.getName() + "'s " + ChatColor.GOLD + "balance is " + ChatColor.YELLOW + bal + "$");
     }
+
+    @CommandHook("pay")
+    public void payCMD(Player p, Player target, int amount) {
+        if (Tazpvp.statsManager.getMoney(p) >= amount && amount > 0) {
+            p.sendMessage(ChatColor.YELLOW + "You paid " + ChatColor.GOLD + amount + "$ " + ChatColor.YELLOW + "to " + ChatColor.GOLD + target.getName());
+            Tazpvp.statsManager.addMoney(p, -amount);
+            Tazpvp.statsManager.addMoney(target, amount);
+            target.sendMessage(ChatColor.YELLOW + "You received " + ChatColor.GOLD + amount + "$ " + ChatColor.YELLOW + "from " + ChatColor.GOLD + p.getName());
+        } else {
+            p.sendMessage(ChatColor.RED + "You don't have enough money!");
+        }
+    }
 }
