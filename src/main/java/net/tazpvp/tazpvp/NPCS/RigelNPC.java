f3984f6dcb8e9e4d28dd83272c5921e0f0935161
rownox <rownox.ik@gmail.com>
2022-06-04 10:25:58 -0400

    nigel

--- src/main/java/net/tazpvp/tazpvp/NPCS/RigelNPC.java
+++ src/main/java/net/tazpvp/tazpvp/NPCS/RigelNPC.java
@@ -1,4 +1,21 @@
 package net.tazpvp.tazpvp.NPCS;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RigelNPC {
+    private static List<Player> clickedOnce = new ArrayList<>();
+
+    public static void clickRigel(Player p){
+        if (p.getWorld().equals(Bukkit.getWorld("arena"))){
+            if (clickedOnce.contains(p)){
+                p.sendMessage("good luck buddy");
+            } else {
+                p.sendMessage("Are you sure you would like to travel the depths?");
+            }
+        }
+    }
 }
