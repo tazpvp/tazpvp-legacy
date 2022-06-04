6651a9937fcb07b807f4c056575b7e221f5e1fdb
rownox <rownox.ik@gmail.com>
2022-06-04 10:45:44 -0400

    spawn

--- src/main/java/net/tazpvp/tazpvp/NPCS/RigelNPC.java
+++ src/main/java/net/tazpvp/tazpvp/NPCS/RigelNPC.java
@@ -2,6 +2,7 @@ package net.tazpvp.tazpvp.NPCS;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -14,9 +15,10 @@ public class RigelNPC {
         if (p.getWorld().equals(Bukkit.getWorld("arena"))){
             if (clickedOnce.contains(p)){
                 p.sendMessage("good luck buddy");
+                p.teleport(new Location(Bukkit.getWorld("ban"), 0.5, 77, 0.5, 0, 0));
                 clickedOnce.remove(p);
             } else {
-                p.sendMessage(ChatColor.DARK_PURPLE + "[NPC] Rigel " + ChatColor.WHITE + "Although this land allows you to rebirth, The Depths is a dangerous place full of god-like enemies. Are you sure? (click again)");
+                p.sendMessage(ChatColor.DARK_PURPLE + "[NPC] Rigel " + ChatColor.LIGHT_PURPLE + "Although this land allows you to rebirth, The Depths is a dangerous place full of god-like enemies. Are you sure? (click again)");
                 clickedOnce.add(p);
             }
         }
