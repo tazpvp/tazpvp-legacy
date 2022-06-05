a12336d436c2ad1d232ab391c8b2a2ba5d65e0ee
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-05 15:55:13 -0400

    fixed setting max health

--- src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
@@ -7,7 +7,7 @@ public class PerkUtils {
     public static void fatPerk(Player p){
         if (!Tazpvp.perkManager.getFatPerk(p)) return;
         if (p.getHealth() > 19) {
-            p.setHealthScale(22);
+            p.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(22);
             p.setHealth(22);
         } else {
             p.setHealth(p.getHealth() + 2);
