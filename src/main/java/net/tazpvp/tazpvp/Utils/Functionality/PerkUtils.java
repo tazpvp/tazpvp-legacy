e2cd99af46ad865f951e66d1f7e76caf9e6bd976
rownox <rownox.ik@gmail.com>
2022-06-05 13:39:46 -0400

    fat perk 2

--- src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
@@ -1,9 +1,11 @@
 package net.tazpvp.tazpvp.Utils.Functionality;
 
+import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.entity.Player;
 
 public class PerkUtils {
     public static void fatPerk(Player p){
+        if (!Tazpvp.perkManager.getFatPerk(p)) return;
         if (p.getHealth() > 19) {
             p.setHealthScale(22);
             p.setHealth(22);
