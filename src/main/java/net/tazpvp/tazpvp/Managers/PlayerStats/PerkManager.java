ca49adf7d73ef511274f44da35aafd24f25ee818
rownox <rownox.ik@gmail.com>
2022-06-06 12:11:24 -0400

    chatcolor

--- src/main/java/net/tazpvp/tazpvp/Managers/PlayerStats/PerkManager.java
+++ src/main/java/net/tazpvp/tazpvp/Managers/PlayerStats/PerkManager.java
@@ -1,6 +1,7 @@
 package net.tazpvp.tazpvp.Managers.PlayerStats;
 
 import net.tazpvp.tazpvp.Tazpvp;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -24,37 +25,37 @@ public class PerkManager {
 
     public boolean getFatPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".fatPerk"); }
     public void setFatPerk(Player p, boolean type){
-        if (getFatPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getFatPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".fatPerk", type);}
 
     public boolean getExcavatorPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".excavatorPerk"); }
     public void setExcavatorPerk(Player p, boolean type){
-        if (getExcavatorPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getExcavatorPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".excavatorPerk", type);}
 
     public boolean getArcherPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".archerPerk"); }
     public void setArcherPerk(Player p, boolean type){
-        if (getArcherPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getArcherPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".archerPerk", type);}
 
     public boolean getBuilderPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".builderPerk"); }
     public void setBuilderPerk(Player p, boolean type){
-        if (getBuilderPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getBuilderPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".builderPerk", type); }
 
     public boolean getGobblePerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".gobblePerk"); }
     public void setGobblePerk(Player p, boolean type){
-        if (getGobblePerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getGobblePerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".gobblePerk", type); }
 
     public boolean getAgilityPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".agilityPerk"); }
     public void setAgilityPerk(Player p, boolean type){
-        if (getAgilityPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getAgilityPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".agilityPerk", type); }
 
     public boolean getTankPerk(Player p){ return statsFile.getBoolean(p.getUniqueId().toString()+".tankPerk"); }
     public void setTankPerk(Player p, boolean type){
-        if (getTankPerk(p)) { p.sendMessage("You already have this perk!"); return;}
+        if (getTankPerk(p)) { p.sendMessage(ChatColor.RED + "You already have this perk!"); return;}
         statsFile.set(p.getUniqueId().toString()+".tankPerk", type); }
 
     public void saveStats(){
