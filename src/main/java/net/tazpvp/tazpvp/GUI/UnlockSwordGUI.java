1d2fd1adb635ddaa9624238bcdfd33e23247124e
rownox <rownox.ik@gmail.com>
2022-06-07 10:21:31 -0400

    fixed ntdi's code 2

--- src/main/java/net/tazpvp/tazpvp/GUI/UnlockSwordGUI.java
+++ src/main/java/net/tazpvp/tazpvp/GUI/UnlockSwordGUI.java
@@ -39,10 +39,11 @@ public class UnlockSwordGUI {
                 Tazpvp.statsManager.addCoins(p, -100);
                 Tazpvp.statsManager.addShards(p, -1);
                 Tazpvp.statsManager.addSpins(p, 1);
-                p.sendMessage(ChatColor.GREEN + "You have bought 1 spin.");
+                p.sendMessage(ChatColor.DARK_AQUA + "You purchased a spin.");
                 p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
             } else {
                 p.sendMessage(ChatColor.RED + "You do not have enough money.");
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_GUITAR, 1, 1);
             }
         });
         gui.addButton(11, buySpins);
@@ -60,11 +61,13 @@ public class UnlockSwordGUI {
             } else {
                 if (Tazpvp.statsManager.getSpins(p) > 0) {
                     Tazpvp.statsManager.setSpins(p, Tazpvp.statsManager.getSpins(p) - 1);
+                    p.sendMessage(ChatColor.DARK_AQUA + "You spun the wheel. What will you get?");
                     spinSword(sword, p);
                     isSpinning = true;
                 } else {
-                    e.getWhoClicked().sendMessage("You don't have any spins!");
+                    e.getWhoClicked().sendMessage(ChatColor.RED + "You don't have any spins!");
                     e.getWhoClicked().closeInventory();
+                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_GUITAR, 1, 1);
                 }
             }
         });
@@ -104,7 +107,7 @@ public class UnlockSwordGUI {
                 } else {
                     Items item = Items.values()[new Random().nextInt(Items.values().length)];
                     sword.setItem(new ItemBuilder(item.getMaterial()).setName(item.getName()).setLore(item.getLore()));
-                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 3, 1);
                     gui.update();
                 }
             }
