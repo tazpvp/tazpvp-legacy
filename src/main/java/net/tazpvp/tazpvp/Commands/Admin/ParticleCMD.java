c4359e751f4154afdc70bb75ded242727668024e
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-08 19:04:52 -0400

    forgot to make particle gui openable

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/ParticleCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/ParticleCMD.java
@@ -1,6 +1,7 @@
 package net.tazpvp.tazpvp.Commands.Admin;
 
 import net.tazpvp.tazpvp.Cosmetics.Particle.Particles.WideCircleParticle;
+import net.tazpvp.tazpvp.GUI.MainMenu.SubMenu.ParticlesGUI;
 import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.Color;
 import org.bukkit.entity.Player;
@@ -9,6 +10,6 @@ import redempt.redlib.commandmanager.CommandHook;
 public class ParticleCMD {
     @CommandHook("particle")
     public void onParticle(Player p, int id, boolean run) {
-
+        new ParticlesGUI(p);
     }
 }
