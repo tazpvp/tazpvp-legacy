83afa0fe9c8d1b06a958a3bf329c726d34649d7a
rownox <rownox.ik@gmail.com>
2022-06-08 18:29:49 -0400

    fixed joeys code

--- src/main/java/net/tazpvp/tazpvp/GUI/NPCGui/ShopGUI.java
+++ src/main/java/net/tazpvp/tazpvp/GUI/NPCGui/ShopGUI.java
@@ -53,7 +53,7 @@ public class ShopGUI {
         createShopButton(new ItemStack(Material.ENDER_EYE, 1), 10, 34, ChatColor.WHITE + "Agility", ChatColor.BLUE + "Speed Boost", false, true, 1.0);
         createShopButton(new ItemStack(Material.GLOW_SQUID_SPAWN_EGG, 1), 11, 8, ChatColor.WHITE + "Extinguisher", ChatColor.BLUE + "Feel the mist", false, true, 2.0);
         createShopButton(new ItemStack(Material.COBWEB, 5), 12, 32, ChatColor.WHITE + "Insta-Web", ChatColor.BLUE + "Slow down enemies.", false, false, null);
-        createShopButton(new ItemStack(Material.ARROW, 5), 13, 13, ChatColor.WHITE + "Arrow", ChatColor.BLUE + "Bow Projectiles", false, false, null);
+        createShopButton(new ItemStack(Material.ARROW, 5), 16, 13, ChatColor.WHITE + "Arrow", ChatColor.BLUE + "Bow Projectiles", false, false, null);
         createShopButton(new ItemStack(Material.OAK_PLANKS, 64), 25, 28, ChatColor.WHITE + "Planks", ChatColor.BLUE + "Placeable Blocks", false, false, null);
         createShopButton(new ItemStack(Material.COOKED_BEEF, 5), 22, 15, ChatColor.WHITE + "Steak", ChatColor.BLUE + "Arbies", false, false, null);
         createShopButton(new ItemStack(Material.GOLDEN_CARROT, 1), 23, 15, ChatColor.WHITE + "Gold Carrot", ChatColor.BLUE + "Healthy Choice", false, false, null);
@@ -68,6 +68,7 @@ public class ShopGUI {
         createShopButton(new ItemStack(Material.GREEN_WOOL, 64), 32, 17, ChatColor.WHITE + "Green Blocks", ChatColor.BLUE + "Bushy", true, false, null);
         createShopButton(new ItemStack(Material.BROWN_WOOL, 64), 33, 17, ChatColor.WHITE + "Brown Blocks", ChatColor.BLUE + "Ew", true, false, null);
         createShopButton(new ItemStack(Material.RED_WOOL, 64), 34, 17, ChatColor.WHITE + "Red Blocks", ChatColor.BLUE + "u mad?", true, false, null);
+        createShopButton(new ItemStack(Material.INK_SAC, 1), 13, 18, ChatColor.WHITE + "Inker", ChatColor.BLUE + "Morb people", false, false, null);
         gui.update();
     }
 }
