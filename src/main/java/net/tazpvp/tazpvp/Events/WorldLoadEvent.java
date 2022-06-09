236e44015f737ca53a033184c9e3884972fb2fdc
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-09 10:54:55 -0400

    eee

--- src/main/java/net/tazpvp/tazpvp/Events/WorldLoadEvent.java
+++ src/main/java/net/tazpvp/tazpvp/Events/WorldLoadEvent.java
@@ -1,6 +1,7 @@
 package net.tazpvp.tazpvp.Events;
 
 import net.tazpvp.tazpvp.Utils.NPCS.Villagers;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +11,7 @@ import static net.tazpvp.tazpvp.Utils.NPCS.NpcUtils.spawn;
 public class WorldLoadEvent implements Listener {
     @EventHandler
     public void onWorldLoad(org.bukkit.event.world.WorldLoadEvent e) {
+        Bukkit.getLogger().info("woreddddd " + e.getWorld().getName());
         World ew = e.getWorld();
         for (Villagers vil : Villagers.values()) {
             World w = vil.location.getWorld();
