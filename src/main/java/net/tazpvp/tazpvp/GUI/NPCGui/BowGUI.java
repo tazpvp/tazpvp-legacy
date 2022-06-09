f2ed0e0c173ce778c6a5eb5b6e2666fe4d77ba70
rownox <rownox.ik@gmail.com>
2022-06-09 12:35:19 -0400

    fixed ntdis code 3

--- src/main/java/net/tazpvp/tazpvp/GUI/NPCGui/BowGUI.java
+++ src/main/java/net/tazpvp/tazpvp/GUI/NPCGui/BowGUI.java
@@ -7,16 +7,19 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import redempt.redlib.inventorygui.InventoryGUI;
 import redempt.redlib.inventorygui.ItemButton;
 import redempt.redlib.itemutils.ItemBuilder;
+
+import static net.tazpvp.tazpvp.Utils.Functionality.IA.ItemStackUtils.hideFlag;
 
 public class BowGUI {
     private InventoryGUI gui;
     String prefix = ChatColor.GREEN + "[NPC] Frank" + ChatColor.DARK_GREEN;
 
     public BowGUI(Player p) {
-        gui = new InventoryGUI(Bukkit.createInventory(null, 4 * 9, ChatColor.BLUE + "" + ChatColor.BLUE + "BOWS AND ARMOR"));
+        gui = new InventoryGUI(Bukkit.createInventory(null, 4 * 9, ChatColor.BLUE + "" + ChatColor.BOLD + "BOWS AND ARMOR"));
         makeItems(p);
         gui.open(p);
     }
@@ -24,28 +27,29 @@ public class BowGUI {
     public void makeItems(Player p) {
         gui.fill(0, 4 * 9, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).setName(" "));
         ItemButton Bow = ItemButton.create(new ItemBuilder(Material.BOW).setName(ChatColor.GOLD + "" + ChatColor.BOLD + "BOW UPGRADES").setLore(ChatColor.GRAY + "Only 1 upgrade can be", ChatColor.GRAY + "selected at a time."), e -> {});
-        ItemButton Armor = ItemButton.create(new ItemBuilder(Material.DIAMOND_CHESTPLATE).setName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "ARMOR UPGRADES").setLore(ChatColor.GRAY + "Only 1 upgrade can be", ChatColor.GRAY + "selected at a time."), e -> {});
+        ItemStack armorMat = new ItemStack(Material.DIAMOND_CHESTPLATE); hideFlag(armorMat);
+        ItemButton Armor = ItemButton.create(new ItemBuilder(armorMat).setName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "ARMOR UPGRADES").setLore(ChatColor.GRAY + "Only 1 upgrade can be", ChatColor.GRAY + "selected at a time."), e -> {});
 
-        ItemButton power = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Power").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "6 Shards"), e -> {
+        ItemButton power = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Power").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "6 Shards"), e -> {
             doChecks(p, Enchantment.ARROW_DAMAGE, 6);
         });
 
-        ItemButton punch = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Punch").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "6 Shards"), e -> {
+        ItemButton punch = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Punch").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "6 Shards"), e -> {
             doChecks(p, Enchantment.ARROW_KNOCKBACK, 6);
         });
 
-        ItemButton flame = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Flame").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "5 Shards"), e -> {
+        ItemButton flame = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Flame").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "5 Shards"), e -> {
             doChecks(p, Enchantment.ARROW_FIRE, 5);
         });
-        ItemButton thorns = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Thorns").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "4 Shards"), e -> {
+        ItemButton thorns = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Thorns").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "4 Shards"), e -> {
             doChecksR(p, Enchantment.THORNS, 1, 4);
         });
 
-        ItemButton prot = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Protection").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "5 Shards"), e -> {
+        ItemButton prot = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Protection").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "5 Shards"), e -> {
             doChecksR(p, Enchantment.PROTECTION_ENVIRONMENTAL, 2, 5);
         });
 
-        ItemButton ff = ItemButton.create(new ItemBuilder(Material.KNOWLEDGE_BOOK).setName(ChatColor.BLUE + "Feather Falling").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "2 Shards"), e -> {
+        ItemButton ff = ItemButton.create(new ItemBuilder(Material.ENCHANTED_BOOK).setName(ChatColor.BLUE + "Feather Falling").setLore(ChatColor.GRAY + "Cost: " + ChatColor.AQUA + "2 Shards"), e -> {
             doChecksR(p, Enchantment.PROTECTION_FALL, 4, 2);
         });
 
