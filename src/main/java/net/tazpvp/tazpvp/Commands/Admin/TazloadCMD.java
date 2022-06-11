4237cde4ad04b94568363fa954884b78dec3805d
rownox <rownox.ik@gmail.com>
2022-06-11 13:22:31 -0400

    tazload msg

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/TazloadCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/TazloadCMD.java
@@ -3,6 +3,7 @@ package net.tazpvp.tazpvp.Commands.Admin;
 import net.tazpvp.tazpvp.Duels.DW;
 import net.tazpvp.tazpvp.Tazpvp;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import redempt.redlib.commandmanager.CommandHook;
 
@@ -12,7 +13,12 @@ import java.util.UUID;
 public class TazloadCMD {
     @CommandHook("tazload")
     public void onTazLoad(Player p) {
-        Bukkit.broadcastMessage("---\nserver restarting in 60s LOL\n---");
+        Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.STRIKETHROUGH + "                                 ");
+        Bukkit.broadcastMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "  RESTART " + ChatColor.WHITE + "Server restart in 60 seconds.");
+        Bukkit.broadcastMessage(ChatColor.WHITE + "");
+        Bukkit.broadcastMessage(ChatColor.GOLD + "   IP: " + ChatColor.YELLOW + "tazpvp.net");
+        Bukkit.broadcastMessage(ChatColor.GOLD + "   DISCORD: " + ChatColor.YELLOW +  "/discord");
+        Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.STRIKETHROUGH + "                                 ");
         for (List<UUID> l : Tazpvp.duelLogic.duels.keySet()) {
             Tazpvp.duelLogic.duelEnd(Bukkit.getPlayer(l.get(0)));
         }
