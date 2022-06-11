518e18c24520306db47b4edb662b1626f77c6cfb
rownox <rownox.ik@gmail.com>
2022-06-11 13:33:31 -0400

    reformat mute

--- src/main/java/net/tazpvp/tazpvp/Commands/Admin/MuteCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Admin/MuteCMD.java
@@ -11,10 +11,10 @@ public class MuteCMD {
     public void mute(CommandSender sender, Player target) {
         if (!Tazpvp.punishmentManager.isMuted(target)) {
             Tazpvp.punishmentManager.initMute(target, true);
-            sender.sendMessage(ChatColor.GOLD + "You have muted " + ChatColor.RED + target.getName());
-            target.sendMessage(ChatColor.GOLD + "You have been muted");
+            sender.sendMessage(ChatColor.DARK_AQUA + "You muted " + ChatColor.AQUA + target.getName());
+            target.sendMessage(ChatColor.DARK_AQUA + "You have been muted");
         } else {
-            sender.sendMessage(ChatColor.RED + target.getName() + ChatColor.GOLD + " is already muted");
+            sender.sendMessage(ChatColor.AQUA + target.getName() + ChatColor.DARK_AQUA + " is already muted");
         }
     }
 
@@ -22,10 +22,10 @@ public class MuteCMD {
     public void unmute(CommandSender sender, Player target) {
         if (Tazpvp.punishmentManager.isMuted(target)) {
             Tazpvp.punishmentManager.removeMute(target);
-            sender.sendMessage(ChatColor.GOLD + "You have unmuted " + ChatColor.RED + target.getName());
-            target.sendMessage(ChatColor.GOLD + "You have been unmuted");
+            sender.sendMessage(ChatColor.DARK_AQUA + "You unmuted " + ChatColor.AQUA + target.getName());
+            target.sendMessage(ChatColor.DARK_AQUA + "You have been unmuted");
         } else {
-            sender.sendMessage(ChatColor.RED + target.getName() + ChatColor.GOLD + " is not muted");
+            sender.sendMessage(ChatColor.AQUA + target.getName() + ChatColor.DARK_AQUA + " is not muted");
         }
     }
 }
