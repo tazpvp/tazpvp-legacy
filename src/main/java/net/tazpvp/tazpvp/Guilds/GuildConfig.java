4d735ac44ab5bb1bfb5ca44b0a9d9d96d1ceaf5a
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-16 22:08:17 -0400

    updated bad words

--- src/main/java/net/tazpvp/tazpvp/Guilds/GuildConfig.java
+++ src/main/java/net/tazpvp/tazpvp/Guilds/GuildConfig.java
@@ -1,17 +1,11 @@
 package net.tazpvp.tazpvp.Guilds;
 
 import net.tazpvp.tazpvp.Tazpvp;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Supplier;
 
 public class GuildConfig {
     public static void reload() {
@@ -35,6 +29,6 @@ public class GuildConfig {
     }
 
     public static List<String> badWords = new ArrayList<String>(Arrays.asList(
-            "anal", "anus","arse","ass","ass","fuck","ass","assfucker","asshole","assshole","bastard","bitch","cock","boong","cock","cockfucker","cocksuck","cocksucker","coon","coonnass","cunt","cyberfuck","dick","douche","erect","erection","erotic","fag","faggot","fuck","fuckass","fuckhole","gook","homoerotic","hore","lesbian","lesbians","motherfuck","motherfucker","negro","nigger","orgasim","orgasm","penis","penisfucker","piss","porn","porno","pornography","pussy","retard","sadist","sex","sexy","shit","slut","bitch","tits","viagra","whore","xxx"
+            "anal", "anus","arse","ass","ass","fuck","ass","assfucker","asshole","assshole","bastard","bitch","cock","boong","cock", "cum","cockfucker","cocksuck","cocksucker","coon","coonnass","cunt","cyberfuck","dick","douche","erect","erection","erotic","fag","faggot","fuck","fuckass","fuckhole","gook","homoerotic","hore","lesbian","lesbians","motherfuck","motherfucker","negro","nigger","orgasim","orgasm","penis","penisfucker","piss","porn","porno","pornography","pussy","retard","sadist","sex","sexy","shit","slut","bitch","tits","viagra","whore","xxx"
     ));
 }
