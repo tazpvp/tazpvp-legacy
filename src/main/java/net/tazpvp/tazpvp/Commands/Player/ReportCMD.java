28b21ce4ac7aac96a73ab3298634280f49842e76
rownox <rownox.ik@gmail.com>
2022-06-18 12:30:23 -0400

    cmd coldown

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import redempt.redlib.commandmanager.CommandHook;
 
-public class ReportCMD implements CommandListener {
+public class ReportCMD implements CommandListener extends CmdCooldown {
     @CommandHook("report")
     public void report(Player p, Player target, String reason) {
 
