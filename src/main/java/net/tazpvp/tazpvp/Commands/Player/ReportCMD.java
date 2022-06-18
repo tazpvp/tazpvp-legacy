dd620ba1c049acf8cb2311037fcfe7e85a55786a
rownox <rownox.ik@gmail.com>
2022-06-18 13:01:19 -0400

    report fix

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
@@ -17,6 +17,10 @@ import redempt.redlib.commandmanager.CommandHook;
 public class ReportCMD extends CmdCooldown {
     @Override
     public void execute(Player p, String[] args) {
+        if (args.length < 2) {
+            p.sendMessage(ChatColor.RED + "Usage: /report <player> <reason>");
+            return;
+        }
         Player target = Bukkit.getPlayer(args[0]);
         String reason = StringUtils.buildString(args, 1);
         if (Tazpvp.hasBeenReported.containsKey(target.getUniqueId())) {
@@ -27,7 +31,6 @@ public class ReportCMD extends CmdCooldown {
             Bukkit.broadcast(ChatColor.GRAY + " " + ChatColor.STRIKETHROUGH + "                                        ", "tazpvp.reports");
             p.sendMessage(ChatColor.DARK_AQUA + "Thank you for reporting " + ChatColor.WHITE + target.getName() + ChatColor.DARK_AQUA + " they will be reviewed shortly.");
         }
-        return;
     }
 
     public ReportCMD() {
