c6902fb1b12d9a05d78cd19c0f8b9259ee1b3d10
rownox <rownox.ik@gmail.com>
2022-06-20 14:34:49 -0400

    oremine

--- src/main/java/net/tazpvp/tazpvp/Utils/Functionality/OreMine.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/Functionality/OreMine.java
@@ -12,6 +12,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class OreMine implements Listener {
     public static void oreRespawn(Player p, Material block, Material block2, Long dur, Block b, BlockBreakEvent e){
+        e.setCancelled(true);
         if (p.getInventory().getItemInMainHand().getType().name().toLowerCase().endsWith("_pickaxe")) {
             ItemStack i = p.getInventory().getItemInMainHand();
             if (i.containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS) && i.containsEnchantment(Enchantment.SILK_TOUCH)){
@@ -26,8 +27,6 @@ public class OreMine implements Listener {
         } else {
             p.getInventory().addItem(new ItemStack(block));
         }
-
-        e.setCancelled(true);
 
         b.setType(Material.BEDROCK);
         new BukkitRunnable() {
