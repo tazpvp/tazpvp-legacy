3cc91cbff955e8fa222e05cd1c4d2cd5dd5bda56
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-07-15 00:48:25 -0400

    fixed reporting missmatch

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/ReportCMD.java
@@ -1,18 +1,11 @@
 package net.tazpvp.tazpvp.Commands.Player;
 
-import net.tazpvp.tazpvp.Commands.CommandListener;
 import net.tazpvp.tazpvp.Tazpvp;
 import net.tazpvp.tazpvp.Utils.CmdCooldown;
 import net.tazpvp.tazpvp.Utils.Functionality.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.jetbrains.annotations.NotNull;
-import redempt.redlib.commandmanager.CommandHook;
 
 public class ReportCMD extends CmdCooldown {
     @Override
@@ -27,7 +20,7 @@ public class ReportCMD extends CmdCooldown {
             p.sendMessage(ChatColor.DARK_AQUA + "Thank you for reporting " + ChatColor.WHITE + target.getName() + ChatColor.DARK_AQUA + " they will be reviewed shortly.");
         } else {
             Bukkit.broadcast(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "                                                                         ", "tazpvp.reports");
-            Bukkit.broadcast(ChatColor.RED + "" + ChatColor.BOLD + "REPORT " + ChatColor.WHITE + target.getName() + ChatColor.GRAY + " reported " + ChatColor.WHITE + p.getName(), "tazpvp.reports");
+            Bukkit.broadcast(ChatColor.RED + "" + ChatColor.BOLD + "REPORT " + ChatColor.WHITE + p.getName() + ChatColor.GRAY + " reported " + ChatColor.WHITE + target.getName(), "tazpvp.reports");
             Bukkit.broadcast(ChatColor.GRAY+ "Reason: " + ChatColor.WHITE + reason, "tazpvp.reports");
             Bukkit.broadcast(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "                                                                         ", "tazpvp.reports");
             p.sendMessage(ChatColor.DARK_AQUA + "Thank you for reporting " + ChatColor.WHITE + target.getName() + ChatColor.DARK_AQUA + " they will be reviewed shortly.");
