76c138a81707e9a6f785b7dbe7fdb7c905bb0b75
rownox <rownox.ik@gmail.com>
2022-07-18 01:16:22 -0400

    duel check

--- src/main/java/net/tazpvp/tazpvp/Commands/Player/DuelCMD.java
+++ src/main/java/net/tazpvp/tazpvp/Commands/Player/DuelCMD.java
@@ -32,7 +32,7 @@ public class DuelCMD {
             p.sendMessage(ChatColor.RED + "You cannot duel as a spectator.");
             return;
         }
-        if (Tazpvp.dueling.containsValue(target.getUniqueId())) {
+        if (Tazpvp.dueling.containsValue(target.getUniqueId()) && !(Tazpvp.dueling.containsValue(p.getUniqueId()))) {
             p.sendMessage(ChatColor.RED + "You have already sent a duel request to " + target.getName() + ".");
             return;
         }
