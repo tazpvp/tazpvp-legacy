5aa4ef57b46aa82298b0bd0c95a46f341372367d
rownox <rownox.ik@gmail.com>
2022-07-24 16:06:25 -0400

    sword when mining

--- src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
+++ src/main/java/net/tazpvp/tazpvp/Utils/Functionality/PerkUtils.java
@@ -1,6 +1,8 @@
 package net.tazpvp.tazpvp.Utils.Functionality;
 
 import net.tazpvp.tazpvp.Tazpvp;
+import net.tazpvp.tazpvp.Utils.Custom.Sword.Items;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -8,6 +10,11 @@ import org.bukkit.inventory.ItemStack;
 import redempt.redlib.itemutils.ItemBuilder;
 
 import java.util.Random;
+
+import static net.tazpvp.tazpvp.Utils.Custom.Sword.GetRandomSword.getRandomSword;
+import static net.tazpvp.tazpvp.Utils.Custom.Sword.ItemBuilder.maekItem;
+import static net.tazpvp.tazpvp.Utils.Functionality.AchieveUtils.Achieve;
+import static net.tazpvp.tazpvp.Utils.Functionality.ChatUtils.sendSurround;
 
 public class PerkUtils {
     static Random rand = new Random();
@@ -18,6 +25,27 @@ public class PerkUtils {
         p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         if (Tazpvp.statsManager.checkLevelUp(p)) {
             Tazpvp.statsManager.levelUp(p, 1);
+            if ((Tazpvp.statsManager.getLevel(p) % 10) == 0) {
+                Items unlockedItem;
+                if (Tazpvp.statsManager.getLevel(p) <= 30) {
+                    unlockedItem = getRandomSword(ChatColor.WHITE + "" + ChatColor.BOLD + "COMMON");
+                } else if (Tazpvp.statsManager.getLevel(p) <= 50) {
+                    unlockedItem = getRandomSword(ChatColor.GREEN + "" + ChatColor.BOLD + "UNCOMMON");
+                } else if (Tazpvp.statsManager.getLevel(p) <= 80) {
+                    unlockedItem = getRandomSword(ChatColor.AQUA + "" + ChatColor.BOLD + "RARE");
+                } else if (Tazpvp.statsManager.getLevel(p) <= 100) {
+                    unlockedItem = getRandomSword(ChatColor.RED + "" + ChatColor.BOLD + "EPIC");
+                } else {
+                    unlockedItem = getRandomSword(ChatColor.GOLD + "" + ChatColor.BOLD + "LEGENDARY");
+                }
+                p.playSound(p.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1, 1);
+                p.sendTitle(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "UNLOCKED", unlockedItem.getName(), 10, 80, 10);
+                sendSurround(p, ChatColor.DARK_AQUA + " You unlocked: " + ChatColor.BOLD + unlockedItem.getName());
+                p.getInventory().addItem(maekItem(unlockedItem));
+                if (Tazpvp.statsManager.getLevel(p) == 100) {
+                    Achieve(p, "Legend", "legend", 8, 235);
+                }
+            }
         }
     }
 
